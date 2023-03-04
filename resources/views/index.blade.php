b818610472db3e62c2b19e2d9aa5857b0f2b207b
Pierre <pierre@iteracode.fr>
2023-03-04 23:05:24 +0100

    [SimpleTimerConfig] Better timer display

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,15 @@
 <x-app-layout>
-    <div class="py-6">
-        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+    <div class="py-2">
+        <div class="max-w-fit px-2">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div id="timer"></div>
+                <div class="p-6 text-gray-900 text-center">
+                    <div id="timer">00.000</div>
                     <button id="action">Start</button>
                 </div>
             </div>
         </div>
     </div>
+
     <x-slot name="scripts">
         <script src="{{ asset('js/timer.js') }}"></script>
     </x-slot>
