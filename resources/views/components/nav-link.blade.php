48de3754c8c11e45724d9d0cafd55fe6d68a0c5b
Pierre <pierre@iteracode.fr>
2023-03-04 18:32:03 +0100

    Initial push / Project stable / Cleaned default laravel files and code

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,14 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'border-indigo-400 text-gray-900 focus:outline-none focus:border-indigo-700'
+            : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300';
+@endphp
+
+<a
+    {{ $attributes->merge([
+        'class' => $classes . 'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out',
+    ]) }}>
+    {{ $slot }}
+</a>
