8b73ec2f6dbf1fd802d519461ce7d181eded4bb8
Pierre <pierre@iteracode.fr>
2023-04-01 23:39:32 +0200

    [Update design profile] Changed index name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex items-center">
                     <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
-                        {{ __('Index') }}
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
             </div>
